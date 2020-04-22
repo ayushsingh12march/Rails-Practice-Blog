2656db1a65a70c3bbf2567136bf60f9ca09e835b
Ayush <ayushsingh12march@gmail.com>
2020-04-22 23:01:30 +0530

    show,create method added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,33 @@
 class ArticlesController < ApplicationController
+
+  #index
+  #show /1
+  #new
+  #edit
+  #create
+  #update
+  #destroy
+
+  #Note :- Rails passes all instance variables to views
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    # @article = Article.new(params[:article])  -> will throw forbidden because of security, specify which params
+    # @article = Article.new(params.require(:article).permit(:title, :text))
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to @article
+    # render plain: params[:article].inspect
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
