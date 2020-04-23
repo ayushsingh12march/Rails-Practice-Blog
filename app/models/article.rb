f073f736c0ce87d1710e6a8de0301cbf5d6cb87f
Ayush <ayushsingh12march@gmail.com>
2020-04-23 16:47:08 +0530

    partial for comment added and destry feature for comment aded

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
-  has_many :comments
+  # if article is deleted all comments also should be deleted --> dependent
+  has_many :comments, dependent: :destroy
   validates :title, presence: true, length: { minimum: 5 }
 end
