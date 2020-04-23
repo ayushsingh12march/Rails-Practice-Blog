93ca701836a4d742eb2d2b681838c1d896f10e5b
Ayush <ayushsingh12march@gmail.com>
2020-04-23 12:12:08 +0530

    rails generate controller Coments & nested comments resource in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'articles/new'
+  get "articles/new"
   get "welcome/index"
 
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  # comments is created as nested resource within articles
 
   root "welcome#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
